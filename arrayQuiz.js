1044676e96b97ca12c7251e5ad30c0794312d1c7
yeonsukkk <119927399+yeonsukkk@users.noreply.github.com>
2023-05-05 14:56:10 +0900

    재사용성 관련 리팩토링

--- arrayQuiz.js
+++ arrayQuiz.js
@@ -5,24 +5,24 @@ console.clear();
 // input: ['🍌', '🍓', '🍇', '🍓']
 // output: [ '🍌', '🥝', '🍇', '🥝' ]
 const quiz1Arr = ['🍌', '🍓', '🍇', '🍓'];
-function quiz1(obj) {
-  const newQuiz1 = obj.map((item) => (item === '🍓' ? '🥝' : item));
+function quiz1(obj, from, to) {
+  const newQuiz1 = obj.map((item) => (item === from ? to : item));
   return newQuiz1;
 }
-console.log('quiz1: ', quiz1(quiz1Arr));
+console.log('quiz1: ', quiz1(quiz1Arr, '🍓', '🥝'));
 
 // 퀴즈2:
 // 배열과 특정한 요소를 전달받아,
 // 배열안에 그 요소가 몇개나 있는지 카운트 하는 함수 만들기
-// input: [ '🍌', '🥝', '🍇', '🥝' ], '🥝'
+// input: [ '🍌', '🥝', '🍇', '🥝' ], '🥝o'
 // output: 2
 const quiz2Arr = ['🍌', '🥝', '🍇', '🥝'];
-function quiz2(obj) {
-  let i = 0;
-  obj.map((item) => (item === '🥝' ? ++i : i));
-  return i;
+function quiz2(obj, countEmoji) {
+  let counter = 0;
+  obj.map((item) => (item === countEmoji ? ++counter : counter));
+  return counter;
 }
-console.log('quiz2: ', quiz2(quiz2Arr));
+console.log('quiz2: ', quiz2(quiz2Arr, '🥝'));
 
 // 퀴즈3: 배열1, 배열2 두개의 배열을 전달받아,
 // 배열1 아이템중 배열2에 존재하는 아이템만 담고 있는 배열 반환
@@ -31,7 +31,16 @@ console.log('quiz2: ', quiz2(quiz2Arr));
 const quiz3Arr1 = ['🍌', '🥝', '🍇'];
 const quiz3Arr2 = ['🍌', '🍓', '🍇', '🍓'];
 function quiz3(obj1, obj2) {
-  const newQuiz3 = obj2.filter((item) => obj1.indexOf(item) !== -1);
+  const newQuiz3 = obj1.filter((item) => obj2.indexOf(item) !== -1);
   return newQuiz3;
 }
+
+function quiz3_2(obj1, obj2) {
+  let result = [];
+  for (let i = 0; i < obj1.length; i++) {
+    if (obj2.includes(obj1[i])) result.push(obj1[i]);
+  }
+  return result;
+}
 console.log('quiz3: ', quiz3(quiz3Arr1, quiz3Arr2));
+console.log('quiz3_2: ', quiz3_2(quiz3Arr1, quiz3Arr2));
